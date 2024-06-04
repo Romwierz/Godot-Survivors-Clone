d19f7afcad003598a27e5b69e347d73a3638fae6
Romwierz <miczu47@gmail.com>
2024-06-04 15:01:20 +0200

    Dodanie animacji postaci i przeciwnika

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -3,11 +3,30 @@ extends CharacterBody2D
 @export var movement_speed = 20.0
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var sprite = $Sprite2D
+#@onready var walkTimer = %WalkTimer
+@onready var animation = $AnimationPlayer
+
+func _ready():
+	animation.play("walk")
 
 # "_" przy delcie oznacza, że nie jest ona wykorzystywana
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	
 	velocity = direction * movement_speed
+	
+	if velocity.x > 0:
+		$Sprite2D.set_flip_h(true)
+	elif velocity.x < 0:
+		sprite.set_flip_h(false)
+	
+	#if velocity != Vector2.ZERO:
+		#if walkTimer.is_stopped():
+			#if sprite.frame >= sprite.hframes - 1:
+				#sprite.frame = 0
+			#else:
+				#sprite.frame = 1
+			#walkTimer.start()
 	
 	move_and_slide()
--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
 
 var movement_speed = 40.0
+@onready var sprite = $Sprite2D
+@onready var walkTimer = get_node("%WalkTimer")
 
-func _physics_process(delta):
-	movement(delta)
+func _physics_process(_delta):
+	movement()
 
-func movement(delta):
+func movement():
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	
 	velocity = direction * movement_speed # nie ma potrzeby normalizowania
+	
+	if velocity.x > 0:
+		$Sprite2D.set_flip_h(true)
+	elif velocity.x < 0:
+		sprite.set_flip_h(false)
+	
+	if velocity != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes - 1:
+				sprite.frame = 0
+			else:
+				sprite.frame = 1
+			walkTimer.start()
+	
+	print(get_velocity().x)
 	
 	move_and_slide() # wbudowana funkcja sprawiająca, że postać się porusza
