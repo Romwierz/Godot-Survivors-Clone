7df32a349bcc824b2320aab8042f74f09a583dde
Romwierz <miczu47@gmail.com>
2024-06-05 12:46:06 +0200

    Zaimplementowanie ataku: IceSpear

--- Player/player.gd
+++ Player/player.gd
@@ -2,8 +2,28 @@ extends CharacterBody2D
 
 @export var movement_speed = 40.0
 @export var hp = 80
+
+# Attacks
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+
+# AttackNodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+
+# Ice Spear
+var icespear_ammo = 0
+var icespear_baseammo = 1
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+# Enemy Related
+var enemy_close = []
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%WalkTimer")
+
+func _ready():
+	attack()
 
 func _physics_process(_delta):
 	movement()
@@ -28,6 +48,45 @@ func movement():
 	
 	move_and_slide() # wbudowana funkcja sprawiająca, że postać się porusza
 
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
+
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print("Player HP:", hp)
+
+func _on_ice_spear_timer_timeout():
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+func _on_ice_spear_attack_timer_timeout():
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+# dodaj przeciwnika do zmiennej enemy_close jeśli jest w obszarze, ale nie ma go w zmiennej
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+# usuń przeciwnika ze zmiennej enemy_close jeśli jest poza obszarem i jest w zmiennej
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)
