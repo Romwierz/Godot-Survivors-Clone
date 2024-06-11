c1fd3b0bcebc91c1072623604b2f0f7c57aa8666
Romwierz <miczu47@gmail.com>
2024-06-11 12:32:29 +0200

    Dodanie bazy do levelowania

--- Player/player.gd
+++ Player/player.gd
@@ -46,6 +46,10 @@ var enemy_close = []
 #GUI
 @onready var expBar = get_node("%ExperienceBar")
 @onready var lblLevel = get_node("%lbl_level")
+@onready var levelPanel = get_node("%LevelUp")
+@onready var upgradeOptions = get_node("%UpgradeOptions")
+@onready var itemOptions = preload("res://Utility/item_option.tscn")
+@onready var sndLevelUp = get_node("%snd_levelup")
 
 func _ready():
 	attack()
@@ -174,14 +178,15 @@ func calculate_experience(gem_exp):
 		# przeliczenie do kolejnego level upu
 		# nie wystarczy że jest to w pierwszej linii funkcji?
 		exp_required = calculate_experiencecap()
+		levelup()
 		calculate_experience(0)
-		#levelup()
 	else:
 		experience += collected_experience
 		collected_experience = 0
 	
 	set_expbar(experience, exp_required)
 
+# oblicza, ile potrzeba exp do level upu
 func calculate_experiencecap():
 	var exp_cap = experience_level
 	if experience_level < 20:
@@ -197,18 +202,78 @@ func set_expbar(set_value = 1, set_max_value = 100):
 	expBar.value = set_value
 	expBar.max_value = set_max_value
 
-#func levelup():
-	#sndLevelUp.play()
-	#lblLevel.text = str("Level: ",experience_level)
-	#var tween = levelPanel.create_tween()
-	#tween.tween_property(levelPanel,"position",Vector2(220,50),0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
-	#tween.play()
-	#levelPanel.visible = true
-	#var options = 0
-	#var optionsmax = 3
-	#while options < optionsmax:
-		#var option_choice = itemOptions.instantiate()
+func levelup():
+	sndLevelUp.play()
+	lblLevel.text = str("Level: ",experience_level)
+	var tween = levelPanel.create_tween()
+	tween.tween_property(levelPanel,"position",Vector2(220,50),0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN)
+	tween.play()
+	levelPanel.visible = true
+	var options = 0
+	var optionsmax = 3
+	while options < optionsmax:
+		var option_choice = itemOptions.instantiate()
 		#option_choice.item = get_random_item()
-		#upgradeOptions.add_child(option_choice)
-		#options += 1
-	#get_tree().paused = true
+		upgradeOptions.add_child(option_choice)
+		options += 1
+	get_tree().paused = true
+
+func upgrade_character(upgrade):
+	match upgrade:
+		"icespear1":
+			icespear_level = 1
+			icespear_baseammo += 1
+		"icespear2":
+			icespear_level = 2
+			icespear_baseammo += 1
+		"icespear3":
+			icespear_level = 3
+		"icespear4":
+			icespear_level = 4
+			icespear_baseammo += 2
+		"tornado1":
+			tornado_level = 1
+			tornado_baseammo += 1
+		"tornado2":
+			tornado_level = 2
+			tornado_baseammo += 1
+		"tornado3":
+			tornado_level = 3
+			tornado_attackspeed -= 0.5
+		"tornado4":
+			tornado_level = 4
+			tornado_baseammo += 1
+		"javelin1":
+			javelin_level = 1
+			javelin_ammo = 1
+		"javelin2":
+			javelin_level = 2
+		"javelin3":
+			javelin_level = 3
+		"javelin4":
+			javelin_level = 4
+		#"armor1","armor2","armor3","armor4":
+			#armor += 1
+		"speed1","speed2","speed3","speed4":
+			movement_speed += 20.0
+		#"tome1","tome2","tome3","tome4":
+			#spell_size += 0.10
+		#"scroll1","scroll2","scroll3","scroll4":
+			#spell_cooldown += 0.05
+		#"ring1","ring2":
+			#additional_attacks += 1
+		#"food":
+			#hp += 20
+			#hp = clamp(hp,0,maxhp)
+	#adjust_gui_collection(upgrade)
+	attack()
+	var option_children = upgradeOptions.get_children()
+	# usunięcie wszystkich children
+	for i in option_children:
+		i.queue_free()
+	#upgrade_options.clear()
+	#collected_upgrades.append(upgrade)
+	levelPanel.visible = false
+	levelPanel.position = Vector2(800, 50)
+	get_tree().paused = false
+	calculate_experience(0)
